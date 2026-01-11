273eb24985c865ef592194c1b992c87d3d409c8e
saintejon <91086180+saintejon@users.noreply.github.com>
2026-01-11 19:53:28 +0100

    fix mobile size

--- src/features/events/ExperiencePage.tsx
+++ src/features/events/ExperiencePage.tsx
@@ -9,18 +9,30 @@ import RegisterForExperience from "./RegisterForExperience";
 const ExperiencePage = ({ experience }: { experience: IEvent }) => {
   return (
     <div className="my-10 gap-4 md:grid md:grid-cols-2 lg:gap-8">
-      <div>
-        <div className="max-w-[300px] md:max-w-[400px]">
+      <div className="mb-10">
+        <div className="md:max-w-[400px]">
           <p className="mb-3 text-3xl font-bold lg:text-5xl">
             {experience.name}
           </p>
-          <div className="flex gap-1">
+          <div className="flex gap-2">
             <div className="bg-primary flex h-fit items-center gap-2 rounded-full p-2">
-              <MapPinIcon className="h-4 w-4 text-black" />
+              <MapPinIcon className="h-3.5 w-3.5 text-black lg:h-4 lg:w-4" />
             </div>
             <div className="flex flex-col space-y-1">
-              <p className="text-xl font-semibold">{experience.venue_name}</p>
+              <p className="text-lg font-semibold md:text-xl">
+                {experience.venue_name}
+              </p>
               <p className="text-sm opacity-80">{experience.address}</p>
+
+              <a
+                href={`https://www.google.com/maps/search/?api=1&query=${experience.latitude},${experience.longitude}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className="mb-2 cursor-pointer text-xs underline opacity-60">
+                  View on Google Maps
+                </p>
+              </a>
             </div>
           </div>
           <div className="space-y-2 py-2 dark:border-gray-800">
